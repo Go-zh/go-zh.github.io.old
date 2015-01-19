1124e13a71650b74b1e9dae39f69c8f9f13655c9
Minux <minux@golang.org>
2015-01-19 01:47:07 -0500

    gerrit: 为新的域名更新配置信息，修正错误的脚本
    Change-Id: Ifea8e8fea688f1e1a0593d85e25c1b759af61c61
    Signed-off-by: Minux <minux@golang.org>

--- gerrit/proxy.go
+++ gerrit/proxy.go
@@ -1,9 +1,8 @@
 //usr/bin/env sudo setcap cap_net_bind_service=+ep proxy; exit
 
 // Gerrit HTTP 服务的反向代理。
-// 本程序监听 :80 端口，并将所有请求 301 到 :443。
-// 同时本程序监听 :443 端口，根据请求的 Host 将请求分发：
-// Gerrit 请求转发 localhost:8000，非 Gerrit 请求转发至 localhost:8080.
+// 本程序监听 :80 端口，将有 SSL 证书的请求 301 到 :443。
+// 同时本程序监听 :443 端口，根据请求的 Host 将请求分发。
 package main
 
 import (
@@ -14,19 +13,52 @@ import (
 	"time"
 )
 
-const GerritHost = "go.minux.info"
+// maps hostname to hostname
+var ReverseProxyTable = map[string]string{
+	"review.go-zh.org": "localhost:8000", // gerrit
+	"go-zh.org":        "localhost:8080", // godoc
+	"blog.go-zh.org":   "localhost:8081", // blog
+	"talks.go-zh.org":  "localhost:8082", // present
+}
+
+// maps hostname to whether we have SSL certificate for that hostname
+var WantHTTPS = map[string]bool{
+	"review.go-zh.org": true,
+	"go-zh.org":        true,
+}
 
 func main() {
+	proxy := &httputil.ReverseProxy{Director: func(req *http.Request) {
+		log.Printf("%s accessing %s", req.RemoteAddr, req.RequestURI)
+		host := ReverseProxyTable[req.Host]
+		if host != "" {
+			*req.URL = url.URL{
+				Scheme: "http",
+				Host:   host,
+				Opaque: req.RequestURI,
+			}
+		} else {
+			*req.URL = url.URL{
+				Scheme: "http",
+				Host:   "localhost:8080",
+				Opaque: req.RequestURI,
+			}
+		}
+	}}
 	go func() {
 		server := &http.Server{
 			Addr: ":80",
 			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				url := url.URL{
-					Scheme: "https",
-					Opaque: "//" + r.Host + r.RequestURI,
+				if WantHTTPS[r.Host] {
+					url := url.URL{
+						Scheme: "https",
+						Opaque: "//" + r.Host + r.RequestURI,
+					}
+					log.Printf("redirects [%v] %s to %s\n", r.RemoteAddr, r.RequestURI, url.String())
+					http.Redirect(w, r, url.String(), http.StatusMovedPermanently)
+				} else {
+					proxy.ServeHTTP(w, r)
 				}
-				log.Printf("redirects [%v] %s to %s\n", r.RemoteAddr, r.RequestURI, url.String())
-				http.Redirect(w, r, url.String(), http.StatusMovedPermanently)
 			}),
 			ReadTimeout:    10 * time.Second,
 			WriteTimeout:   10 * time.Second,
@@ -35,24 +67,8 @@ func main() {
 		log.Fatal(server.ListenAndServe())
 	}()
 	server := &http.Server{
-		Addr: ":443",
-		Handler: &httputil.ReverseProxy{Director: func(r *http.Request) {
-			log.Printf("[%s] accessing %s", r.RemoteAddr, r.RequestURI)
-			switch r.Host {
-			case GerritHost:
-				*r.URL = url.URL{
-					Scheme: "http",
-					Host:   "localhost:8000",
-					Opaque: r.RequestURI,
-				}
-			default:
-				*r.URL = url.URL{
-					Scheme: "http",
-					Host:   "localhost:8080",
-					Opaque: r.RequestURI,
-				}
-			}
-		}},
+		Addr:           ":443",
+		Handler:        proxy,
 		ReadTimeout:    15 * time.Second,
 		WriteTimeout:   15 * time.Second,
 		MaxHeaderBytes: 1 << 16,
